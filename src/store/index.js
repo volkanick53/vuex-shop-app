e9cb623bf79b9ac1bc7deeb925d7ca30bd6bb48b
Volkan <Volkan@DESKTOP-2AVO8VN>
2024-02-06 18:29:21 +0300

    Setup vuex and modified

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import {createStore} from 'vuex';
+import productsModule from './modules/products.js'
+import cartModule from './modules/cart.js'
+
+const store = createStore({
+    modules: {
+        prods: productsModule,
+        cart: cartModule
+    }
+
+
+})
+
+
+export default store;
